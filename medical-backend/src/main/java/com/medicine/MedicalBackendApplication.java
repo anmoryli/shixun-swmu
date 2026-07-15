b7f8264f0dd311ff32f19ff774725a841b8cd441
Spike-star <wangdachui030317@gmail.com>
2026-07-15 10:04:13 +0800

    feat(backend): enable dashboard cache infrastructure

--- medical-backend/src/main/java/com/medicine/MedicalBackendApplication.java
+++ medical-backend/src/main/java/com/medicine/MedicalBackendApplication.java
@@ -7,9 +7,13 @@ package com.medicine;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan({"com.medicine.auth.mapper", "com.medicine.business.mapper", "com.medicine.dashboard.mapper"})
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class MedicalBackendApplication {
 
     public static void main(String[] args) {
